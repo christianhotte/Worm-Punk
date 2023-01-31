b71c9170ba99bb1e1f3a4478f17781db619dc10e
AlmostcoolFSU <44479998+AlmostcoolFSU@users.noreply.github.com>
2023-01-31 14:37:18 -0500

    Starting Hinges
    Hinge exists and can be moved with colliders but not gravity

---
+++ Assets/Scripts/BarrelLock.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelLock : MonoBehaviour
+{
+    public FirstGunScript fgs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fgs = GetComponentInParent<FirstGunScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("oof");
+        if (other.gameObject.tag == "Barrel" && fgs.Ejecting)
+        {
+            Debug.Log("shutting");
+            fgs.CloseBreach();
+        }
+    }
+}
---
+++ Assets/Scripts/FirstGunScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstGunScript : MonoBehaviour
+{
+    public ConfigurableJoint breakJoint;
+    private PlayerInput input;
+
+    public bool Ejecting = false;
+    [SerializeField, Range(0, 90), Tooltip("Angle at which barrels will rest when breach is open")] private float breakAngle;
+
+    private void Awake()
+    {
+        input = GetComponentInParent<PlayerInput>();
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Eject();
+        //StartCoroutine(WaitandClose());
+        SoftJointLimit angleCap = new SoftJointLimit();
+        angleCap = breakJoint.highAngularXLimit;
+        Debug.Log(angleCap.limit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    public void Eject()
+    {
+        Ejecting = true;
+        Debug.Log("ejecting");
+        breakJoint.angularXMotion = ConfigurableJointMotion.Limited;
+        breakJoint.targetRotation = Quaternion.Euler(Vector3.right * breakAngle);
+    }
+    public void CloseBreach()
+    {
+        if (!Ejecting) return;
+        breakJoint.targetRotation = Quaternion.Euler(Vector3.zero);
+        //float num = breakJoint.co
+        breakJoint.angularXMotion = ConfigurableJointMotion.Locked; 
+        Ejecting = false;
+    }   
+    public IEnumerator WaitandClose()
+    {
+        yield return new WaitForSeconds(5f);
+        Debug.Log("closing");
+        CloseBreach();
+
+    }
+}
