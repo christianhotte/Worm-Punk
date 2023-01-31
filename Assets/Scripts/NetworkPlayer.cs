5904d3b23ad869fd9853d6d7558053e587e0aa24
SakuretsuKumare <Dawncoming3@gmail.com>
2023-01-31 15:17:36 -0500

    Rigs Move Network Player
    Before, the XR Origin moved on the network, but the Network Player moved locally. Everything is synced now.  Added some basic materials to slap onto objects.

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using Photon.Pun;
 
 // This script was used from https://youtu.be/KHWuTBmT1oI?t=1511
@@ -13,38 +14,59 @@ public class NetworkPlayer : MonoBehaviour
     public Transform leftHand;
     public Transform rightHand;
 
+    private GameObject XROrigin;
     private PhotonView photonView;
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
 
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+
+        // Gets the network player to move with the player instead of just moving locally.
+        XROrigin = GameObject.Find("XR Origin");
+        headRig = XROrigin.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = XROrigin.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = XROrigin.transform.Find("Camera Offset/RightHand Controller");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Only shows the player over the network instead of on the client; only your player
+        // Synchronizes the player over the network.
         if (photonView.IsMine)
         {
+            /* Disables the network player's camera & hands so that it uses the XR Origin's.
             rightHand.gameObject.SetActive(false);
             leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
+            head.gameObject.SetActive(false);*/
 
             // Calls these functions to map the position of the player's hands & headset
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            // Disables all of the renderers in the Network player so that we can just render the XR Origin's.
+            if (photonView.IsMine)
+            {
+                foreach (var item in GetComponentsInChildren<Renderer>())
+                {
+                    item.enabled = false;
+                }
+            }
         }
     }
 
     // This synchronizes the positions of the headset & the hand controllers 
-    void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, Transform rigTransform)
     {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+        /*InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);*/
 
-        target.position = position;
-        target.rotation = rotation;
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
     }
 }
