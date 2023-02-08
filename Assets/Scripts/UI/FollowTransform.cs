66a39d738d184d0aff8896d55c1e012962958bb9
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-02-08 13:55:54 -0500

    AnyUI Package, UI Follow

---
+++ Assets/Scripts/UI/FollowTransform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private Transform transformToFollow;
+    [SerializeField] private float followSpeed;
+
+    private Transform objectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectTransform.LookAt(lookAt, Vector3.up);
+        //objectTransform.Rotate(0f, 180f, 0f);
+
+        Vector3 newPos = objectTransform.position;
+        Vector3 followPos = transformToFollow.position;
+        newPos.x = Mathf.Lerp(newPos.x, followPos.x, followSpeed * Time.deltaTime);
+        newPos.y = Mathf.Lerp(newPos.y, followPos.y, followSpeed * Time.deltaTime);
+        newPos.z = Mathf.Lerp(newPos.z, followPos.z, followSpeed * Time.deltaTime);
+        transform.position = newPos;
+    }
+}
