c543c13d5ade971841eb72c5c01717ec118c179f
SakuretsuKumare <Dawncoming3@gmail.com>
2023-02-14 15:23:59 -0500

    Worm Pods & Levers
    New closed worm pod prefab all with levers. Network player is able to spawn in every room besides the Main Menu scene. Converted network player spawner to work on DontDestroyOnLoad.

--- Assets/Scripts/Networking/AutoJoinRoom.cs
+++ Assets/Scripts/Networking/AutoJoinRoom.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AutoJoinRoom : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // For debugging purposes. Drag & drop this script to anything in the scene to auto join a room.
         NetworkManagerScript.instance.joinRoomOnLoad = true;
