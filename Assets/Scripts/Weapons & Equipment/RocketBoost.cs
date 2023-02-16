f47eee14021f57878b0c3b5b0d3fafa5d1a91870
AlmostcoolFSU <44479998+AlmostcoolFSU@users.noreply.github.com>
2023-02-16 16:13:55 -0500

    Update RocketBoost.cs
    Fixed Grapple hitting your own hitbox

--- Assets/Scripts/Weapons & Equipment/RocketBoost.cs
+++ Assets/Scripts/Weapons & Equipment/RocketBoost.cs
@@ -38,7 +38,7 @@ public class RocketBoost : PlayerEquipment  //renametograpple
             realDistance = Vector3.Distance(rocketTip.position, HookInstance.transform.position); // gets distance to the hit
             rayToHitDistance = Vector3.Distance(rocketTip.position, HookInstance.transform.position);
             RaycastHit checkSaw;
-            var sawRay = Physics.Raycast(rocketTip.position, rocketTip.forward, out checkSaw,9999999, ~LayerMask.GetMask("PlayerWeapon"));
+            var sawRay = Physics.Raycast(rocketTip.position, rocketTip.forward, out checkSaw,9999999, ~LayerMask.GetMask("PlayerWeapon","Player"));
             rayToHitDistance = 999;
 
             if (checkSaw.collider == null) return;
