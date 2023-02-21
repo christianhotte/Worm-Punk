68a69f2717bb85b53e8f13126237984fdd93a239
SakuretsuKumare <Dawncoming3@gmail.com>
2023-02-21 13:59:08 -0500

    Locker Room Spawner take 2
    Modified so that it only takes the local player's demo player on joined room.

--- Assets/Scripts/Networking/SpawnManager2.cs
+++ Assets/Scripts/Networking/SpawnManager2.cs
@@ -2,38 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class SpawnManager2 : MonoBehaviour
+public class SpawnManager2 : MonoBehaviourPunCallbacks
 {
     public Transform[] spawnPoints; // An array of spawn points for the players
 
     private int nextSpawnPointIndex = 0; // Index of the next spawn point to use
 
-    // Start is called before the first frame update
-    void Start()
+    public override void OnJoinedRoom()
     {
-        if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient)
+        // Move the demo player to the appropriate spawn point
+        MoveDemoPlayerToSpawnPoint(PhotonNetwork.LocalPlayer);
+    }
+
+    private void MoveDemoPlayerToSpawnPoint(Player player)
+    {
+        // Get the demo player for this player's client
+        GameObject demoPlayer = player.TagObject as GameObject;
+        if (demoPlayer != null)
         {
-            // Spawn the players when the game starts
-            SpawnPlayer();
+            // Get the position and rotation of the next spawn point
+            Transform spawnPoint = spawnPoints[nextSpawnPointIndex];
+            Vector3 spawnPosition = spawnPoint.position;
+            //Quaternion spawnRotation = spawnPoint.rotation;
+
+            // Move the demo player to the spawn point
+            demoPlayer.transform.position = spawnPosition;
+            //demoPlayer.transform.rotation = spawnRotation;
+
+            // Update the next spawn point index
+            UpdateSpawnPoint();
         }
     }
 
-    private void SpawnPlayer()
+    //On left room as well.
+
+    private void UpdateSpawnPoint()
     {
-        // Get the position and rotation of the next spawn point
-        Transform spawnPoint = spawnPoints[nextSpawnPointIndex];
-        Vector3 spawnPosition = spawnPoint.position;
-        Quaternion spawnRotation = spawnPoint.rotation;
-
-        // Spawn the Network Player at the spawn point
-        //PhotonNetwork.Instantiate("NetworkPlayer", spawnPosition, spawnRotation);
-
-        // Move the demo player to the spawn point
-        GameObject demoPlayer = GameObject.Find("DemoPlayer4");
-        demoPlayer.transform.position = spawnPosition;
-        demoPlayer.transform.rotation = spawnRotation;
-
         // Update the next spawn point index
         nextSpawnPointIndex = (nextSpawnPointIndex + 1) % spawnPoints.Length;
     }
