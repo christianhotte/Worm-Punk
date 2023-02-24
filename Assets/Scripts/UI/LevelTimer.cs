17e56d07a2626de6ba8fb341ede859565f08a4e6
pgomesProjects <70992905+pgomesProjects@users.noreply.github.com>
2023-02-24 14:36:17 -0500

    Round Length Room Setting

--- Assets/Scripts/UI/LevelTimer.cs
+++ Assets/Scripts/UI/LevelTimer.cs
@@ -7,25 +7,52 @@ using Photon.Pun;
 
 public class LevelTimer : MonoBehaviour
 {
-    private int levelTime;
-    private float currentTime;
+    private int levelTime;  //The desired timer for the level
+    private float currentTime;  //The current time left
 
-    private TextMeshProUGUI timerText;
+    private TextMeshProUGUI timerText;  //The timer text component
 
-    private bool timerEnded;
-    public UnityEvent OnTimerEnd;
+    private bool timerActive;   //If true, the timer is active. If false, the timer is not active
+    private bool timerEnded;    //If true, the timer has ended. If false, the timer has not ended
+
+    public UnityEvent OnTimerEnd;  //The event to call when the timer ends
 
     private void Start()
     {
-        SetLevelTime(300);
         timerText = GetComponent<TextMeshProUGUI>();
+        timerActive = false;
 
-        currentTime = levelTime;
+        ActivateTimer();
     }
 
     private void OnEnable()
     {
+        if (!timerActive)
+            ActivateTimer();
+
         OnTimerEnd.AddListener(BackToLockerRoom);
+    }
+
+    /// <summary>
+    /// Gets the room's round length and applies it to the timer.
+    /// </summary>
+    private void ActivateTimer()
+    {
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            Debug.Log("Round Length: " + (int)PhotonNetwork.CurrentRoom.CustomProperties["RoundLength"]);
+            if ((int)PhotonNetwork.CurrentRoom.CustomProperties["RoundLength"] < 0)
+            {
+                timerActive = false;
+                timerText.color = new Color(0, 0, 0, 0);
+            }
+            else
+            {
+                SetLevelTime((int)PhotonNetwork.CurrentRoom.CustomProperties["RoundLength"]);
+                currentTime = levelTime;
+                timerActive = true;
+            }
+        }
     }
 
     private void OnDisable()
@@ -35,27 +62,39 @@ public class LevelTimer : MonoBehaviour
 
     private void Update()
     {
-        if(currentTime > 0)
+        //If the timer is active
+        if (timerActive)
         {
-            currentTime -= Time.deltaTime;
-            DisplayTime();
-        }
-        else if(!timerEnded)
-        {
-            currentTime = 0;
-            DisplayTime();
+            //If there is time left, decrement the timer
+            if (currentTime > 0)
+            {
+                currentTime -= Time.deltaTime;
+                DisplayTime();
+            }
+            //If there is no time, end the timer and call the subscribed event(s)
+            else if (!timerEnded)
+            {
+                currentTime = 0;
+                DisplayTime();
 
-            Debug.Log("Time Is Up!");
-            OnTimerEnd.Invoke();
-            timerEnded = true;
+                Debug.Log("Time Is Up!");
+                OnTimerEnd.Invoke();
+                timerEnded = true;
+            }
         }
     }
 
+    /// <summary>
+    /// Displays the time in minutes and seconds.
+    /// </summary>
     private void DisplayTime()
     {
         timerText.text = GetMinutes() + ":" + GetSeconds();
     }
 
+    /// <summary>
+    /// Brings all of the players in the scene back to the locker room.
+    /// </summary>
     public void BackToLockerRoom()
     {
         if (PhotonNetwork.IsMasterClient)
