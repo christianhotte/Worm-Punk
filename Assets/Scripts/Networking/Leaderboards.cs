d704c5ec6c77aaa66962960f0e5b85f4577cad61
SakuretsuKumare <Dawncoming3@gmail.com>
2023-03-01 10:11:33 -0500

    Testing Leaderboard Script
    Changed the scene name to have the name of the last scene to the full Asseets/name.. Game Manager comments changed

--- Assets/Scripts/Networking/Leaderboards.cs
+++ Assets/Scripts/Networking/Leaderboards.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 using TMPro;
 using System.Linq;
@@ -15,13 +16,13 @@ public class Leaderboards : MonoBehaviourPunCallbacks
     void Start()
     {
         playerScores = "";
-        arenaScene = "DM_0.12_Arena";
+        arenaScene = "Assets/Scenes/DMars_Scenes/DM_0.12_Arena.unity";
 
         // Gets the name of the last scene we were on (returning from the arena to the network locker room).
-        GameManager gameManager = FindObjectOfType<GameManager>();
+        GameManager gameManager = GameManager.Instance;
         string lastSceneName = gameManager.GetLastSceneName();
 
-        Debug.Log(lastSceneName);
+        Debug.Log("Last Scene Name: " + lastSceneName);
 
         if (lastSceneName == arenaScene)
         {
@@ -31,6 +32,7 @@ public class Leaderboards : MonoBehaviourPunCallbacks
         else
         {
             gameObject.SetActive(false);
+            Debug.Log("God fucking dammit");
         }
     }
 
